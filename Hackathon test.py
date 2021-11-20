a86b107f1c809fc9888a6bf3dea6cdf6ed2f0c45
souyeds <94550606+souyeds@users.noreply.github.com>
2021-11-20 17:49:24 +0530

    Add files via upload

---
+++ Hackathon test.py
@@ -0,0 +1,7 @@
+
+#Welcome to hackathon project created by SOURAV DEY
+import cv2
+dir_path = "C:\Python\SD PROJECT\FINANCIAL MARKET WORKSPACE\SD WORKSPACE\GUI App Desktop\.venv\Assets\Photo or Images\""
+file_name = (input("Enter file name and it's extension:"))
+#Joining File name , so that entire path we get
+assets_path = print("".join([dir_path, file_name])) 
